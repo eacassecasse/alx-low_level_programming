08431e09181855f79bc9713f300b803a25575fc1
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-14 12:23:12 -0700

    REFACTOR Reducing the code to meet the 40 lines per function requirement

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,31 +17,20 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			if (i < j)
+
+			comb = i * 10 + j;
+
+			(comb >= 10) ? putchar('0' + comb / 10) : putchar('0');
+
+			putchar('0' + comb % 10);
+
+			if (comb < 89)
 			{
-
-				comb = i * 10 + j; 
-
-
-				if (comb >= 10)
+				for (k = 0; k < length; k++)
 				{
-					putchar('0' + comb / 10);
-				}
-				else
-				{
-					putchar('0');
-				}
-
-				putchar('0' + comb % 10);
-
-				if (comb < 89)
-				{
-					for (k = 0; k < length; k++)
-					{
-						putchar(separator[k]);
-					}
+					putchar(separator[k]);
 				}
 			}
 
