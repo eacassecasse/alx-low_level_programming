71b5aa296567928a109fbb68a4ab6e674849576f
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-16 13:20:46 -0700

    REFACTOR Reducing the 100-times_table.c to fit 40 lines function requirement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,7 +24,7 @@ void print(int n)
  */
 void print_times_table(int n)
 {
-	if (n > 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		int i, j;
 
@@ -62,8 +62,6 @@ void print_times_table(int n)
 			_putchar(10);
 		}
 	}
-	else if (n == 0)
-		_putchar('0');
 }
 
 
