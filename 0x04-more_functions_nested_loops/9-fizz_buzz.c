6664dbf9fb43ec96b9fb2c2cfa39de25cf41e315
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-17 12:52:22 -0700

    FIX Verifying if the number is divisible by 3 and 5 first, rather than verifying if its divisible by 3 first

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,12 +13,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			(i < 100) ? printf("Fizz ") : printf("Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			(i < 100) ? printf("FizzBuzz ") : printf("FizzBuzz");
 		else if (i % 5 == 0)
 			(i < 100) ? printf("Buzz ") : printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			(i < 100) ? printf("FizzBuzz ") : printf("FizzBuzz");
+		else if (i % 3 == 0)
+			(i < 100) ? printf("Fizz ") : printf("Fizz");
 		else
 			(i < 100) ? printf("%d ", i) : printf("%d", i);
 	}
