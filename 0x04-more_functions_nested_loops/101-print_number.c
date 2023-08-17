b16016bbf5a61348502e4b94bc5a8e932446e38e
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-17 14:41:56 -0700

    FIX Adding the absolute value of INT_MIN instead of calculating it

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,11 +19,8 @@ void print_number(int n)
 
 	if (n == INT_MIN)
 	{
-		int absVal;
-
-		absVal = -(INT_MAX) - 1;
 		_putchar('-');
-		print_number(absVal);
+		print_number(147483648);
 		return;
 	}
 
