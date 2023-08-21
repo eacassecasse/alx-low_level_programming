d2a872d75ea59ac5a06e6674a0c4e4fb5413bd7e
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-21 16:13:32 -0700

    FIX Changing verification from != to <

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,7 +18,7 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		(i != n - 1) ? printf("%d, ", *(a + i)) :
+		(i < n - 1) ? printf("%d, ", *(a + i)) :
 			printf("%d\n", *(a + i));
 		i++;
 	}
