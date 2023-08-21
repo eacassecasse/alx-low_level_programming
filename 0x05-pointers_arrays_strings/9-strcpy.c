26c34d458524116816b7e0e4a3265e6aced32806
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-21 15:54:12 -0700

    FEATURE _strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strcpy - Custom function
+ * @dest: pointer to the first param to swap
+ * @src: pointer to the second param to swap
+ *
+ * Description: Copies a string to the buffer
+ * Return: char *
+ */
+char * _strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return dest;
+}
