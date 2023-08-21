d3699bb65bf9507f9d142de6c656edcf3214ff2b
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-21 14:58:31 -0700

    FIX Fiting betty requirements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,12 @@ void print_array(int *a, int n)
 {
 
 	int i;
-	
+
 	i = 0;
 
 	while (i < n)
 	{
-		(i != n-1) ? printf("%d, ", *(a + i)):
+		(i != n - 1) ? printf("%d, ", *(a + i)) :
 			printf("%d\n", *(a + i));
 		i++;
 	}
