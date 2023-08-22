7694c766dbc944d871c4da44b71ff07ed5198986
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-22 09:43:23 -0700

    FEATURE Adding a program that creates a random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints the largest prime factor of a given number
+ * Return: 0 - Always Success
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += pass[i];
+		putchar(pass[i] + '0');
+
+		if ((2772 - sum) < 78)
+		{
+			n = 2772 - sum;
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
