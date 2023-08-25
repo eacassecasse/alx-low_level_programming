9ef4fc99803215511ff3ae9dd12452d9d3d99569
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-08-25 16:21:47 -0700

    FIX Trying to resolve bugs

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
-
 /**
- * infinite_add - Custom function
- * @n1: pointer to the first number to add
- * @n2: pointer to the second number to add
- * @r: pointer to the result of the addition
- * @size_r: size of the result buffer
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer where result is stored
+ * @size_r: size of the buffer/
  *
- * Description: Sum 2 numbers
- * Return: The result buffer or 0 if the result cannot be stored in the buffer
+ * Return: 0 always
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
@@ -23,13 +20,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	i--, j--;
 	for (k = 0; i >= 0 || j >= 0 || of == 1; i--, j--, k++)
 	{
-		(i < 0) ? (num1 = 0) : (num1 = n1[i] - '0');
-		(j < 0) ? (num2 = 0) : (num2 = n2[j] - '0');
-
+		if (i < 0)
+			num1 = 0;
+		else
+			num1 = n1[i] - '0';
+		if (j < 0)
+			num2 = 0;
+		else
+			num2 = n2[j] - '0';
 		sum = num1 + num2 + of;
-		
-		(sum > 9) ? (of = 1) : (of = 0);
-
+		if (sum > 9)
+			of = 1;
+		else
+			of = 0;
 		if (k >= size_r - 1)
 			return (0);
 		r[k] = (sum % 10) + '0';
