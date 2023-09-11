fc966eff8861979bec0304c5b77177704bc17c70
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-09-11 13:33:08 -0700

    REFACTOR Betty, coding style

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef SIZE
+#ifndef OBjECT_LIKE_MACRO_H
+#define OBJECT_lIKE_MACRO_H
+
 #define SIZE 1024
 
-#endif /* #ifndef SIZE */
+#endif /* #ifndef OBJECT_LIKE_MACRO_H */
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef ABS
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
 #define ABS(x) (((x) >= 0) ? (x) : ((x) * (-1)))
 
-#endif /* #ifndef ABS(x) */
+#endif /* #ifndef FUNCTION_LIKE_MACRO_H */
