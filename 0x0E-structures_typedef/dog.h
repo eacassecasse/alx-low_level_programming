81ff5d23723c6e65e3ca4bcdc861e7ef915a107d
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2023-09-11 19:14:21 -0700

    FIX Adding semi-colon after struct definition and struct keyword after typedef on dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
-/**
- * dog_t - Typedef for dog
- */
-typedef dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
